4f37c8f0f9acba9ec9c2242fc38f5ceff3e5eb9d
Sir1u5 <zod15ru@yandex.ru>
2023-07-28 01:32:16 +0300

    8 спринт гланый экран/настройки

---
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,21 @@
+package com.example.playlistmaker
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SettingsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+
+        val backFromSettings = findViewById<ImageView>(R.id.sittingBack)
+
+        backFromSettings.setOnClickListener{
+            val backSettingsIntent = Intent (this, MainActivity::class.java)
+            startActivity(backSettingsIntent)
+        }
+    }
+}
